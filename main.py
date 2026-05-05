778f4dbf66ee64adad7158030b0046a943999c98
Ali <aliabdulaev770@gmail.com>
2026-05-05 23:23:41 +0300

    fixes for dev

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from typing import Union
 def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     """Функция, которая складывает два числаФункция, которая складывает два числа"""
     return a + b
+# изменения
